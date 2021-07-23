918e58f9dd15c10d99bdd0941795641b63fe236f
Your Name <jasjohn3@uat.edu>
2021-07-23 10:36:15 -0700

    fetch request function complete

--- assets/js/current.js
+++ assets/js/current.js
@@ -1,16 +1,15 @@
 let apiKey ='9c4fb1938de2911a2efa8d463ac83a48'
-
-let citySearchForm = document.getElementById('city-search-form');
+let searchSubmit = document.getElementById('submit-search');
 // api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=9c4fb1938de2911a2efa8d463ac83a48
 console.log(apiKey);
-console.log(citySearchForm);
+console.log(searchSubmit);
 function openweatherFetchRequest(e){
   e.preventDefault();
   alert('Submit Button Pressed');
   let search = document.getElementById('search');
   let searchValue = search.value;
   console.log(searchValue);
-  let fetchAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=${apiKey}`
+  let fetchAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=imperial&appid=${apiKey}`
   
   fetch(fetchAPIURL)
   .then(response => response.json())
@@ -18,5 +17,5 @@ function openweatherFetchRequest(e){
    console.log(data); 
   })
   .catch(err =>err);
-  citySearchForm.submit();
 }
+searchSubmit.addEventListener('click',openweatherFetchRequest);
