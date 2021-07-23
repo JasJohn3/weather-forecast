8c71180345c6414bce4f52ebc34cc38555b88fca
Your Name <jasjohn3@uat.edu>
2021-07-23 08:35:35 -0700

    creating functions for api call

--- assets/js/current.js
+++ assets/js/current.js
@@ -1,6 +1,22 @@
 let apiKey ='9c4fb1938de2911a2efa8d463ac83a48'
-let searchValue = document.getElementById('search');
-let searchSubmit = document.getElementById('submit-search');
+
+let citySearchForm = document.getElementById('city-search-form');
+// api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=9c4fb1938de2911a2efa8d463ac83a48
 console.log(apiKey);
-console.log(searchValue);
-console.log(searchSubmit.value);
+console.log(citySearchForm);
+function openweatherFetchRequest(e){
+  e.preventDefault();
+  alert('Submit Button Pressed');
+  let search = document.getElementById('search');
+  let searchValue = search.value;
+  console.log(searchValue);
+  let fetchAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=${apiKey}`
+  
+  fetch(fetchAPIURL)
+  .then(response => response.json())
+  .then(data => {
+   console.log(data); 
+  })
+  .catch(err =>err);
+  citySearchForm.submit();
+}
