046f8478dedbf51acf9393e5c0bdd77f5a044beb
Your Name <jasjohn3@uat.edu>
2021-07-24 00:28:33 -0700

    Simplified Weather App html and renamed to dashboard

--- assets/js/forecast.js
+++
@@ -1,6 +0,0 @@
-let apiKey ='9c4fb1938de2911a2efa8d463ac83a48'
-let searchValue = document.getElementById('search');
-let searchSubmit = document.getElementById('submit-search');
-console.log(apiKey);
-console.log(searchValue);
-console.log(searchSubmit.value);
