b35259199020a3bd3554b485d09464f6636a531a
Your Name <jasjohn3@uat.edu>
2021-07-24 00:17:14 -0700

    Search History function completed

--- assets/js/current.js
+++ assets/js/current.js
@@ -5,17 +5,49 @@ console.log(apiKey);
 console.log(searchSubmit);
 function openweatherFetchRequest(e){
   e.preventDefault();
-  alert('Submit Button Pressed');
   let search = document.getElementById('search');
   let searchValue = search.value;
-  console.log(searchValue);
+  localStorageHistory(searchValue);
   let fetchAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=imperial&appid=${apiKey}`
   
   fetch(fetchAPIURL)
   .then(response => response.json())
-  .then(data => {
-   console.log(data); 
+  .then(data => { recipeResponse(data);
+   
   })
   .catch(err =>err);
 }
+recipeResponse=(data)=>{
+  console.log(data); 
+}
+localStorageHistory =(store_data)=>{
+  // Local Storage Steps
+  let localHistory =JSON.parse(localStorage.getItem('city'))||[];
+  localHistory.push(store_data);
+  localStorage.setItem('city',JSON.stringify(localHistory));
+  createSearchHistoryEl(localHistory);
+  // Local Storage Steps
+}
+createSearchHistoryEl=(localHistory)=>{
+  let uniqueHistory = [...new Set(localHistory)];
+  uniqueHistory.splice(10);
+  console.log(uniqueHistory);
+  searchHistoryUL = document.getElementById('search-history');
+  searchHistoryUL.innerHTML ='';
+  uniqueHistory.forEach(item =>{
+    console.log(item);
+    liEl = document.createElement('li');
+    liEl.classList.add("list-group-item");
+    liEl.classList.add('text-dark');
+    console.log(liEl)
+    // liEl.className("list-group-item text-dark");
+    liEl.textContent =item;
+    searchHistoryUL.append(liEl);
+  });
+}
+loadLocalHistory=()=>{
+  let localHistory =JSON.parse(localStorage.getItem('city'))||[];
+  createSearchHistoryEl(localHistory);
+};
+loadLocalHistory();
 searchSubmit.addEventListener('click',openweatherFetchRequest);
